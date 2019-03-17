43def1b8f1a7c58958676264b421187fdce68ec0
Lewis Chung <lewis@shopwith.co>
2019-03-16 17:07:51 -0700

    handle shipping rates and payment

--- lib/shopify/resources/payment.ex
+++ lib/shopify/resources/payment.ex
@@ -1,42 +1,108 @@
 defmodule Shopify.Payment do
   @derive [Poison.Encoder]
-  @singular "checkout"
-  @plural "checkouts"
+  @singular "payment"
+  @plural "payments"
 
   use Shopify.Resource,
     import: [
-      :find,
       :all,
-      :count,
-      :create
+      :find,
+      :create,
+      :update,
+      :count
     ]
 
   alias Shopify.{
-    Payment
+    Checkout
   }
 
   defstruct [
-    :request_details,
-    :amount,
-    :session_id,
-    :credit_card,
-    :id,
-    :payment_processing_error_message,
-    :transaction,
-    :unique_token
+    :abandoned_checkout_url,
+    :billing_address,
+    :buyer_accepts_marketing,
+    :cancel_reason,
+    :cart_token,
+    :closed_at,
+    :completed_at,
+    :created_at,
+    :currency,
+    :customer,
+    :discount_codes,
+    :email,
+    :gateway,
+    :landing_site,
+    :line_items,
+    :note,
+    :phone,
+    :referring_site,
+    :shipping_address,
+    :shipping_line,
+    :source_name,
+    :subtotal_price,
+    :tax_lines,
+    :taxes_included,
+    :token,
+    :total_discounts,
+    :total_line_items_price,
+    :total_price,
+    :total_tax,
+    :total_weight,
+    :updated_at,
+    :order,
+    :order_id,
+    :customer_id,
+    :location_id,
+    :device_id,
+    :shopify_payments_account_id,
+    :user_id
   ]
 
   @doc false
   def empty_resource do
-    %Payment{}
+    %Checkout{
+      billing_address: %BillingAddress{},
+      shipping_address: %ShippingAddress{},
+      customer: %Customer{
+        default_address: %Address{}
+      },
+      line_items: [
+        %LineItem{
+          tax_lines: [%TaxLine{}]
+        }
+      ],
+      shipping_line: %ShippingLine{},
+      tax_lines: [%TaxLine{}],
+      discount_codes: [%DiscountCode{}],
+      order: %Checkout.Order{}
+    }
   end
-
-  @doc false
-  def find_url(id), do: @plural <> "/#{id}.json"
 
   @doc false
   def all_url, do: @plural <> ".json"
 
   @doc false
+  def find_url(token), do: @plural <> "/#{token}.json"
+
+  @doc false
   def count_url, do: @plural <> "/count.json"
+
+  @doc """
+  Creates a payment on a checkout using the session ID returned by the card vault.
+
+  Returns `{:ok, %Shopify.Response{}}` or `{:error, %Shopify.Response{}}`
+
+  ## Parameters
+    - session: A `%Shopify.Session{}` struct.
+    - token: A checkout token.
+
+  ## Examples
+      iex> Shopify.session |> Shopify.Checkout.payments("asdfasdfasdf")
+      {:ok, %Shopify.Response{}}
+  """
+  @spec payments(%Shopify.Session{}, binary) :: {:ok, %__MODULE__{}} | {:error, map}
+  def payments(session, token) do
+    session
+    |> Request.new("checkouts" <> "/#{token}/payments.json", empty_resource())
+    |> Client.post()
+  end
 end
---
+++ lib/shopify/resources/shipping_rates.ex
@@ -0,0 +1,51 @@
+defmodule Shopify.ShippingRate do
+  @derive [Poison.Encoder]
+  @singular "shipping_rate"
+  @plural "shipping_rates"
+
+  use Shopify.Resource,
+    import: [
+      :all
+    ]
+
+  alias Shopify.{
+    ShippingRate
+  }
+
+  defstruct [
+    :id,
+    :price,
+    :title,
+    :checkout,
+    :phone_required,
+    :delivery_range,
+    :handle
+  ]
+
+  @doc false
+  def empty_resource do
+    %ShippingRate{
+      checkout: %Checkout{}
+    }
+  end
+
+  @doc """
+  Retrieves a list of shipping rates for a token.
+
+  Returns `{:ok, %Shopify.Response{}}` or `{:error, %Shopify.Response{}}`
+
+  ## Parameters
+    - session: A `%Shopify.Session{}` struct.
+    - token: A checkout token.
+
+  ## Examples
+      iex> Shopify.session |> Shopify.ShippingRates.all("asdfasdfasdf")
+      {:ok, %Shopify.Response{}}
+  """
+  @spec shipping_rates(%Shopify.Session{}, binary) :: {:ok, %__MODULE__{}} | {:error, map}
+  def all(session, token) do
+    session
+    |> Request.new(@plural <> "/#{token}/shipping_rates.json", empty_resource())
+    |> Client.get()
+  end
+end
